dc57bb31802161d95aa372952f432318e90e7f0d
MyLifeConsort <iqrachaudhary347@gmail.com>
2025-08-16 21:19:46 -0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+pluginManagement {
+
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StudentBiometricLog"
+include(":app")
+ 
